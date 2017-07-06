ae4a5438333e5a5e127c24bc3cd7df9b99f5e57e
Travis <travissheppard.1988@gmail.com>
2017-07-05 19:27:35 -0700

    more example stuff

--- js/app.js
+++ js/app.js
@@ -4,29 +4,41 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import 'waypoints';
 import 'scrollTo';
 
-/**
- * EXAMPLE JS STARTS
- */
+import React from 'react';
+import { render } from 'react-dom';
+import Graph from './graph';
+
+const TARGET_EL_ID = 'j-target';
+
 $(function() {
-    $('[id^=scrollTo]').click(function() {
-        var id = $(this).attr('id').slice(9);
-        $(window).scrollTo($('#' + id), 1000, { offset: { top: -51, left: 0 } });
-    });
+  /**
+   * EXAMPLE JS STARTS
+  */
+  let optional_data = { nodes: [], edges: [] };
+  render(
+    <Graph data={optional_data} />,
+    document.getElementById(TARGET_EL_ID)
+  );
+  /**
+   * EXAMPLE JS ENDS
+  */
+  $('[id^=scrollTo]').click(function() {
+    var id = $(this).attr('id').slice(9);
+    $(window).scrollTo($('#' + id), 1000, { offset: { top: -51, left: 0 } });
+  });
 
-    $('#marketing').waypoint(function() {
-        $('.img-circle').addClass('animated zoomIn');
-    }, {
-        offset: '50%',
-        triggerOnce: true
-    });
+  $('#marketing').waypoint(function() {
+    $('.img-circle').addClass('animated zoomIn');
+  }, {
+    offset: '50%',
+    triggerOnce: true
+  });
 
-    $('.featurette').waypoint(function() {
-        $('#' + this.element.id + ' .featurette-image').addClass('animated pulse');
-    }, {
-        offset: '50%',
-        triggerOnce: true
-    });
+  $('.featurette').waypoint(function() {
+    $('#' + this.element.id + ' .featurette-image').addClass('animated pulse');
+  }, {
+    offset: '50%',
+    triggerOnce: true
+  });
 });
-/**
- * EXAMPLE JS ENDS
- */
+
